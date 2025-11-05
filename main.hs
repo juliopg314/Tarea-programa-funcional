6c7eefef75ebc446c32ef8026950ce152689a0e8
juliopg314 <juliopg@ciencias.unam.mx>
2025-11-05 10:48:25 -0600

    Update main.hs

--- main.hs
+++ main.hs
@@ -1,4 +1,4 @@
-
+-- definición de la función 'quicksort' 
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort (p:xs) = quicksort menores ++ [p] ++ quicksort mayores
@@ -6,12 +6,16 @@ quicksort (p:xs) = quicksort menores ++ [p] ++ quicksort mayores
         menores = filter (<= p) xs
         mayores = filter (> p) xs
 
+-- punto de entrada 'main' 
+-- 'main' es la acción que se ejecuta al correr el programa.
+-- Su tipo 'IO ()' indica que realiza acciones de Input/Output (como imprimir).
 main :: IO ()
 main = do
-    
+    -- Definimos una lista de ejemplo
     let listaOriginal = [7, 2, 9, 1, 5, 10, 3]
     
+    -- Llamamos a nuestra función 'quicksort' con esa lista
     let listaOrdenada = quicksort listaOriginal
     
-   
+    -- Usamos 'print' para imprimir el resultado en la consola
     print listaOrdenada
